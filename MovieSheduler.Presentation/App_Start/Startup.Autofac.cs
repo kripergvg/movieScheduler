6cbe57740379da4a22dfb13659013321f3eaa0dd
Владимир Иванов <wkololo_4ever@mail.ru>
2016-06-23 12:41:47 +0300

    refactoring

--- MovieSheduler.Presentation/App_Start/Startup.Autofac.cs
+++ MovieSheduler.Presentation/App_Start/Startup.Autofac.cs
@@ -9,44 +9,41 @@ using MovieSheduler.Domain.Infrastructure;
 using MovieSheduler.EntityFramework;
 using MovieSheduler.EntityFramework.Infrastructure;
 using MovieSheduler.EntityFramework.Repositories.SheduleRecord;
-using MovieSheduler.Presentation.Controllers;
 using MovieSheduler.Presentation.Core.Messager;
 
 namespace MovieSheduler.Presentation
 {
-	public partial class Startup
-	{
-	    public IContainer ConfigureAutofac()
-	    {
-	        var builder=new ContainerBuilder();
+    public partial class Startup
+    {
+        public IContainer ConfigureAutofac()
+        {
+            var builder = new ContainerBuilder();
 
             builder.Register(c => new HttpContextWrapper(HttpContext.Current)).As<HttpContextBase>().InstancePerRequest();
 
-            builder.RegisterControllers(typeof (MvcApplication).Assembly);
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
 
-            //TODO коммент
-	        builder.RegisterAssemblyTypes(typeof (SheduleRecordService).Assembly)
-	            .Where(t => t.Name.EndsWith("Service"))
-	            .AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(typeof(SheduleRecordService).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces();
 
-            //TODO коммент
             builder.RegisterAssemblyTypes(typeof(SheduleRecordRepository).Assembly)
                 .Where(t => t.Name.EndsWith("Repository"))
                 .AsImplementedInterfaces();
 
-	        builder.RegisterType<MovieShedulerContext>().InstancePerLifetimeScope()
+            builder.RegisterType<MovieShedulerContext>().InstancePerLifetimeScope()
                 .As<DbContext>()
                 .As<MovieShedulerContext>();
 
-	        builder.RegisterType<UnitOfWorkFactory>().As<IUnitOfWorkFactory>();
-	        builder.RegisterType<ValidationDictionary>().As<IValidationDictionary>();
-	        builder.RegisterType<Notifier>().As<INotifier>().InstancePerRequest();
-	        builder.Register(b => new NotifierFilterAttribute(b.Resolve<INotifier>())).AsActionFilterFor<Controller>().InstancePerRequest();
+            builder.RegisterType<UnitOfWorkFactory>().As<IUnitOfWorkFactory>();
+            builder.RegisterType<ValidationDictionary>().As<IValidationDictionary>();
+            builder.RegisterType<Notifier>().As<INotifier>().InstancePerRequest();
+            builder.Register(b => new NotifierFilterAttribute(b.Resolve<INotifier>())).AsActionFilterFor<Controller>().InstancePerRequest();
             builder.RegisterFilterProvider();
 
-	        IContainer container = builder.Build();
-	        DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-	        return container;
-	    }
-	}
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            return container;
+        }
+    }
 }
